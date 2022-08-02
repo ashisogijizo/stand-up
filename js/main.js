b470ef1023bcd14e3305317ce10d8035adffffca
ashisogijizo <109790993+ashisogijizo@users.noreply.github.com>
2022-08-02 19:04:21 +0600

    slider, swiper, responsive to 1350px

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var slider = $('.slider'),
+	wrapper = $('#wrapper'),
+	animating = false,
+	current = 0,
+	lengthDiv = slider.length,
+	delay = 300;
+
+slider.on('click', function(e){
+	var anchor = $(this);
+	if(!animating){
+		animating = true;
+		current = anchor.parent().index();
+        wrapper.removeClass().addClass('slide'+current);
+        setTimeout(function(){
+            animating = false;
+        }, delay);
+		e.preventDefault();
+	}
+});
+
+$(document).keydown(function(e){var key = e.keyCode;if(key == 38 || key == 40)e.preventDefault();});
+$(document).keyup(function(e){
+	if(!animating){
+		var key = e.keyCode;
+		if(key == 38 && current > 0){
+            $(slider[current - 1]).trigger('click');
+		}else if(key == 40 && current < lengthDiv - 1){
+            $(slider[current + 1]).trigger('click');
+		}
+	}
+});
+
+document.addEventListener('wheel', e => {
+    if (e.deltaY < 0) {
+        $(slider[current - 1]).trigger('click');
+    } else if(e.deltaY > 0 && current < lengthDiv - 1){
+        $(slider[current + 1]).trigger('click');
+    }
+    $(slider[current]).addClass('active');
+    $(slider[current-1]).removeClass('active');
+    $(slider[current+1]).removeClass('active');
+
+});
+const swiper = new Swiper('.events-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: false,
+    allowTouchMove: true,
+    grabCursor: true,
+    centerInsufficientSlides: true,
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true
+    },
+  });
+
